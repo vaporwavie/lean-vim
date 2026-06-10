81c109c7283475201032c8b04ac200b71554e77e
Luiz Nickel <luizaugustonickel@gmail.com>
2026-06-09 22:52:34 -0300

    feat(treesitter): migrate to nvim-treesitter main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
--- nvim-treesitter configuration for syntax and textobjects
+-- nvim-treesitter (main branch) + textobjects for Neovim 0.11+
 local add, do_now = MiniDeps.add, MiniDeps.now
 
 add {
   source = "nvim-treesitter/nvim-treesitter",
-  checkout = "master",
+  checkout = "main",
   hooks = {
     post_checkout = function()
       vim.cmd [[ :TSUpdate ]]
@@ -11,42 +11,78 @@ add {
   },
 }
 
--- Separate add so nvim-treesitter is on the rtp before textobjects'
--- plugin/*.vim auto-requires `nvim-treesitter.configs`.
-add { source = "nvim-treesitter/nvim-treesitter-textobjects", checkout = "master" }
+add { source = "nvim-treesitter/nvim-treesitter-textobjects", checkout = "main" }
 
 do_now(function()
   -- stylua: ignore
   local language_grammars = {
     "astro", "javascript", "typescript", "tsx", "css", "html", "scss",
     "cmake", "cpp", "c",
-    "lua", "bash", "xml", "markdown",
+    "lua", "bash", "xml", "markdown", "markdown_inline",
   }
-  require("nvim-treesitter.configs").setup {
-    ensure_installed = language_grammars,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { "markdown" },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-        goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-      },
-    },
+
+  require("nvim-treesitter").setup {
+    install_dir = vim.fn.stdpath "data" .. "/site",
   }
+  require("nvim-treesitter").install(language_grammars)
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+      "astro",
+      "javascript",
+      "typescript",
+      "typescriptreact",
+      "javascriptreact",
+      "tsx",
+      "css",
+      "html",
+      "scss",
+      "cmake",
+      "cpp",
+      "c",
+      "lua",
+      "bash",
+      "sh",
+      "xml",
+      "markdown",
+      "markdown_inline",
+    },
+    callback = function()
+      pcall(vim.treesitter.start)
+    end,
+  })
+
+  require("nvim-treesitter-textobjects").setup {
+    select = { lookahead = true },
+    move = { set_jumps = true },
+  }
+
+  local select = function(capture)
+    return function()
+      require("nvim-treesitter-textobjects.select").select_textobject(capture, "textobjects")
+    end
+  end
+  local goto_next = function(capture)
+    return function()
+      require("nvim-treesitter-textobjects.move").goto_next_start(capture, "textobjects")
+    end
+  end
+  local goto_prev = function(capture)
+    return function()
+      require("nvim-treesitter-textobjects.move").goto_previous_start(capture, "textobjects")
+    end
+  end
+
+  local map = vim.keymap.set
+  map({ "x", "o" }, "af", select "@function.outer")
+  map({ "x", "o" }, "if", select "@function.inner")
+  map({ "x", "o" }, "ac", select "@class.outer")
+  map({ "x", "o" }, "ic", select "@class.inner")
+  map({ "x", "o" }, "aa", select "@parameter.outer")
+  map({ "x", "o" }, "ia", select "@parameter.inner")
+
+  map({ "n", "x", "o" }, "]f", goto_next "@function.outer")
+  map({ "n", "x", "o" }, "]c", goto_next "@class.outer")
+  map({ "n", "x", "o" }, "[f", goto_prev "@function.outer")
+  map({ "n", "x", "o" }, "[c", goto_prev "@class.outer")
 end)
